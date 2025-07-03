f423097185b97b0de846c40d22453f30d31dcad9
Akshara <23wh1a6613@bvrithyderabad.edu.in>
2025-07-03 22:28:08 +0530

    .

---
+++ petStore/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Navbar from './components/navbar'
+import Home from './pages/Home'
+import {Route,Routes} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+    
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+
+    </>
+  )
+}
+
+export default App
