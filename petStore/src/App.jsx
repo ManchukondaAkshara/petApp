4c2c1cb36cc2f71b081f0a207c1829eb1b9ed2f1
lokesh-22 <lokeshboddupally13@gmail.com>
2025-07-04 17:27:42 +0530

    Added Navbar

--- petStore/src/App.jsx
+++ petStore/src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css'
-import Navbar from './components/navbar'
+
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import {Route,Routes} from 'react-router-dom'
 
